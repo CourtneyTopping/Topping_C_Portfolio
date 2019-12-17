9296a934796088c64327abd55d0b3e1516bffd18
Courtney Topping <Courtney@Courtneys-MacBook-Pro.local>
2019-12-17 11:17:37 -0500

    ready for pool

--- utils/sql.js
+++ utils/sql.js
@@ -1,12 +1,15 @@
 const config = require("../config");
 const sql = require('mysql');
 
-const connect = sql.createConnection({
-    host: config.host,
-    port: config.port,
-    user: config.uname,
-    password: config.pword,
-    database: config.database
+const connect = sql.createPool({
+  host: config.host,
+  //port: config.port,
+  user: config.user,
+  password: config.password,
+  database: config.database,
+  connectionLimit : 20,
+  queueLimit : 100,
+  waitForConnections : true
 })
 
 module.exports = connect;
